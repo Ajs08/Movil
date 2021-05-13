c9c556d900ff93a7ec837e73ed097f9151382336
jdcastro11 <jdcastro11@gmail.com>
2021-05-12 23:23:16 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/motoposadapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.motoposadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private final String contra = "pass123";
+    private final String user = "jdcastro";
+    public static  final String EXTRA_USUARIO = "com.example.motoposadapp.USUARIO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void  ingresarUsuario(View view){
+        //Validar campos en blanco
+        EditText usuario = findViewById(R.id.edtUsuario);
+        EditText pass = findViewById(R.id.edtPassword);
+        String txtnombre = usuario.getText().toString();
+        String txtcontra = pass.getText().toString();
+        if (txtnombre.isEmpty()){
+            usuario.setError("El nombre no puede estar en vacio");
+        }else{
+            if (txtcontra.isEmpty()){
+                pass.setError("Contraseña vacia");
+            }else{
+                if (txtcontra.equals(contra) && txtnombre.equals(user)){
+                    Intent intent = new Intent(this, Activity2.class);
+                    intent.putExtra(EXTRA_USUARIO, txtnombre);
+                    startActivity(intent);
+                }
+                else{
+                    pass.setError("Contraseña invalida");
+                }
+            }
+        }
+
+
+    }
+}
