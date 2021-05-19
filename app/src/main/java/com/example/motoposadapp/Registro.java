83cb4c85922e971e3d75b51edb2702840e2716e8
Albert Salas <alberth0804@hotmail.com>
2021-05-18 21:10:10 -0500

    DatePicker creacion

--- app/src/main/java/com/example/motoposadapp/Registro.java
+++ app/src/main/java/com/example/motoposadapp/Registro.java
@@ -2,18 +2,25 @@ package com.example.motoposadapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class Registro extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        initDatePicker();
+        dateButton = findViewById(R.id.BotonCalendario);
 
         TextView btn = (TextView)findViewById(R.id.YaTieneCuenta);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +29,23 @@ public class Registro extends AppCompatActivity {
                 startActivity(new Intent(Registro.this, MainActivity.class));
             }
         });
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dataSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+            }
+        };
+    }
+
+    private String makeDateString(int day, int month, int year) {
+    }
+
+    public void openDatePicker(View view) {
     }
 }
