1219b4445828582545c1820e113f64a9d4dfdf96
Albert Salas <alberth0804@hotmail.com>
2021-05-18 22:35:08 -0500

    DatePick, asi se va

--- app/src/main/java/com/example/motoposadapp/Registro.java
+++ app/src/main/java/com/example/motoposadapp/Registro.java
@@ -2,6 +2,7 @@ package com.example.motoposadapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Registro extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class Registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
         initDatePicker();
         dateButton = findViewById(R.id.BotonCalendario);
+        dateButton.setText(getTodaysDate());
 
         TextView btn = (TextView)findViewById(R.id.YaTieneCuenta);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +35,17 @@ public class Registro extends AppCompatActivity {
         });
     }
 
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return makeDateString(day, month, year);
+
+    }
+
     private void initDatePicker() {
         DatePickerDialog.OnDateSetListener dataSetListener = new DatePickerDialog.OnDateSetListener()
         {
@@ -39,13 +54,57 @@ public class Registro extends AppCompatActivity {
             {
                 month = month + 1;
                 String date = makeDateString(day, month, year);
+                dateButton.setText(date);
             }
         };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        DatePickerDialog.OnDateSetListener dateSetListener = null;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
     }
 
     private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
     }
 
-    public void openDatePicker(View view) {
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "ENE";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "ABR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AGO";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DIC";
+
+        return "ENE";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
     }
 }
