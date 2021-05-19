6b37b27296ec8a1d1af0b05c942240692f1e5f94
Albert Salas <alberth0804@hotmail.com>
2021-05-18 19:49:15 -0500

    Listados Listos

--- app/src/main/java/com/example/motoposadapp/ListadoActivity.java
+++ app/src/main/java/com/example/motoposadapp/ListadoActivity.java
@@ -3,12 +3,91 @@ package com.example.motoposadapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ListadoActivity extends AppCompatActivity {
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+    MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
+
+        expandableListView = findViewById(R.id.ListaDesplegable);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new MainAdapter(this,listGroup,listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+    }
+
+    private void initListData() {
+        listGroup.add(getString(R.string.departamento1));
+        listGroup.add(getString(R.string.departamento2));
+        listGroup.add(getString(R.string.departamento3));
+        listGroup.add(getString(R.string.departamento4));
+        listGroup.add(getString(R.string.departamento5));
+        listGroup.add(getString(R.string.departamento6));
+        listGroup.add(getString(R.string.departamento7));
+
+        String[] array;
+
+        List<String> list1 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento1);
+        for (String item: array){
+            list1.add(item);
+        }
+
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento2);
+        for (String item: array){
+            list2.add(item);
+        }
+
+        List<String> list3 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento3);
+        for (String item: array){
+            list3.add(item);
+        }
+
+        List<String> list4 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento4);
+        for (String item: array){
+            list4.add(item);
+        }
+
+        List<String> list5 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento5);
+        for (String item: array){
+            list5.add(item);
+        }
+
+        List<String> list6 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento6);
+        for (String item: array){
+            list6.add(item);
+        }
+
+        List<String> list7 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.departamento7);
+        for (String item: array){
+            list7.add(item);
+        }
+
+        listItem.put(listGroup.get(0), list1);
+        listItem.put(listGroup.get(1), list2);
+        listItem.put(listGroup.get(2), list3);
+        listItem.put(listGroup.get(3), list4);
+        listItem.put(listGroup.get(4), list5);
+        listItem.put(listGroup.get(5), list6);
+        listItem.put(listGroup.get(6), list7);
+        adapter.notifyDataSetChanged();
     }
 }
