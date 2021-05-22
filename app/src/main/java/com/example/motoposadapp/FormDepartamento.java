65653863bde6f363c98f03416363063e625e7b02
luz Villar <luzvf18@gmail.com>
2021-05-21 23:50:12 -0500

    merge ciudad y departamentos en alter

---
+++ app/src/main/java/com/example/motoposadapp/FormDepartamento.java
@@ -0,0 +1,14 @@
+package com.example.motoposadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormDepartamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_departamento);
+    }
+}
